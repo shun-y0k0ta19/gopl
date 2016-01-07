c7810f5cefc5aeee8bb6bf746a699afb3b49c57d
y0k0ta19 <motoyk@gmail.com>
2016-01-07 14:08:59 +0900

    first commit

---
+++ CH01/ex01/echo.go
@@ -0,0 +1,18 @@
+// Author: "Shun Yokota"
+// Copyright © 2016 RICOH Co, Ltd. All rights reserved
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
