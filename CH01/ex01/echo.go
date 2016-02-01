8c8f5be1486834f04ed0c5a9ded0fd5a9ceeb45e
y0k0ta19 <motoyk@gmail.com>
2016-02-02 00:31:20 +0900

    use strings.Join()

--- CH01/ex01/echo.go
+++ CH01/ex01/echo.go
@@ -6,13 +6,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	s, sep := "", ""
-	for _, arg := range os.Args[0:] {
-		s += sep + arg
-		sep = " "
-	}
-	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[0:], " "))
 }
