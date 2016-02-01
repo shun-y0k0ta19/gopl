94359c99cb19c33407d009baef0ac4787937f9c1
y0k0ta19 <motoyk@gmail.com>
2016-02-01 23:29:47 +0900

    use range loop

--- CH01/ex01/echo.go
+++ CH01/ex01/echo.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	var s, sep string
-	for i := 0; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	s, sep := "", ""
+	for _, arg := range os.Args[0:] {
+		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
