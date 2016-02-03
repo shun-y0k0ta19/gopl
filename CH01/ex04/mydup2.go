81c010bd344f818ec84ecc8b1fb99c01083b285b
y0k0ta19 <motoyk@gmail.com>
2016-02-04 07:54:10 +0900

    modifying

---
+++ CH01/ex04/mydup2.go
@@ -0,0 +1,47 @@
+// Author: "Shun Yokota"
+// Copyright © 2016 RICOH Co, Ltd. All rights reserved
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type set map[string]struct{}
+
+var filenames = make(set)
+var includedFiles = make(map[string]set)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, "stdin")
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, arg)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\t\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int, filename string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		counts[line]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
