36aee54c0a2ca7a91df8f924c44bbadaae4874db
y0k0ta19 <motoyk@gmail.com>
2016-02-08 19:26:36 +0900

    first commit

---
+++ CH01/ex07/myfetch.go
@@ -0,0 +1,27 @@
+// Author: "Shun Yokota"
+// Copyright © 2016 RICOH Co, Ltd. All rights reserved
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
---
+++ CH01/ex08/myfetch2.go
@@ -0,0 +1,31 @@
+// Author: "Shun Yokota"
+// Copyright © 2016 RICOH Co, Ltd. All rights reservedpackage main
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
