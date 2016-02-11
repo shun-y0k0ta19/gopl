9371d9613edf2da1550ee6c16655542d6447ba48
y0k0ta19 <motoyk@gmail.com>
2016-02-12 03:14:21 +0900

    imple withStringJoin and withoutStringJoin

--- CH01/ex03/measStringJoin.go
+++ CH01/ex03/measStringJoin.go
@@ -1,19 +1,37 @@
 // Author: "Shun Yokota"
 // Copyright © 2016 RICOH Co, Ltd. All rights reserved
 
-package main
+//result
+/*
+go test -bench .
+PASS
+BenchmarkWithoutJoin-4	      10	 132607999 ns/op
+BenchmarkWithJoin-4   	    5000	    296768 ns/op
+ok  	golang_training/CH01/ex03	3.132s
+*/
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+package measStringJoin
 
-func main() {
-	s, sep := "", ""
-	for i, arg := range os.Args[1:] {
-		s += sep + strconv.Itoa(i) + " " + arg
-		sep = "\n"
+import "strings"
+
+var ss []string
+
+func init() {
+	for i := 0; i < 20000; i++ {
+		ss = append(ss, "a")
 	}
-	fmt.Println(s)
+}
+
+func withoutJoin() string {
+	s, sep := "", ""
+	for _, arg := range ss {
+		s += sep + arg
+		sep = " "
+	}
+	return s
+}
+
+func withJoin() string {
+	s := strings.Join(ss, " ")
+	return s
 }
