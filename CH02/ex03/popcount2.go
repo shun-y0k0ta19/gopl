dbc40907e28e8169389a26ad059b9b421320ebe2
y0k0ta19 <motoyk@gmail.com>
2016-02-11 23:27:50 +0900

    first commit

---
+++ CH02/ex03/popcount2.go
@@ -0,0 +1,21 @@
+// Author: "Shun Yokota"
+// Copyright © 2016 RICOH Co, Ltd. All rights reserved
+
+package popcount2
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+//PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	var res int
+	for i := uint64(0); i < 8; i++ {
+		res += int(pc[byte(x>>(i*8))])
+	}
+	return res
+}
