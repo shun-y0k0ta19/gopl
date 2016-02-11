5fe3450e5508771cfd57553a41d9db8e47eef8e6
y0k0ta19 <motoyk@gmail.com>
2016-02-12 02:14:25 +0900

    imple file output

--- CH01/ex10/myfetchall.go
+++ CH01/ex10/myfetchall.go
@@ -1,6 +1,20 @@
 // Author: "Shun Yokota"
 // Copyright © 2016 RICOH Co, Ltd. All rights reserved
 
+//result
+/*
+y0k0ta19:ex10 y0k0ta19$ gor myfetchall.go gopl.io golang.org godoc.org
+0.47s     7839  http://golang.org
+0.85s     6368  http://godoc.org
+1.73s     4146  http://gopl.io
+1.73s elapsed
+y0k0ta19:ex10 y0k0ta19$ gor myfetchall.go gopl.io golang.org godoc.org
+0.29s     4146  http://gopl.io
+0.38s     7839  http://golang.org
+0.80s     6368  http://godoc.org
+0.80s elapsed
+
+*/
 package main
 
 import (
@@ -9,19 +23,32 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
+	resfile, err := os.Create("result.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "myfetchall: %v\n", err)
+		os.Exit(1)
+	}
+
 	start := time.Now()
 	ch := make(chan string)
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		go fetch(url, ch) // start a goroutine
 	}
 	for range os.Args[1:] {
-		fmt.Println(<-ch) // receive from channel ch
+		chres := <-ch
+		fmt.Println(chres) // receive from channel chan type
+		fmt.Fprintln(resfile, chres)
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	resfile.Close()
 }
 
 func fetch(url string, ch chan<- string) {
