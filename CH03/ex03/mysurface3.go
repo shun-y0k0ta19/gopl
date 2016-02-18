a164f07f4d109574e18773d590dd9fc8d9ea74eb
y0k0ta19 <motoyk@gmail.com>
2016-02-18 21:02:59 +0900

    "impl color change (must fix minus bug)"

--- CH03/ex03/mysurface3.go
+++ CH03/ex03/mysurface3.go
@@ -46,25 +46,27 @@ func outputSVG(z zf) {
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay, isNaNa := corner(i+1, j, z)
-			bx, by, isNaNb := corner(i, j, z)
-			cx, cy, isNaNc := corner(i, j+1, z)
-			dx, dy, isNaNd := corner(i+1, j+1, z)
+			ax, ay, za, isNaNa := corner(i+1, j, z)
+			bx, by, zb, isNaNb := corner(i, j, z)
+			cx, cy, zc, isNaNc := corner(i, j+1, z)
+			dx, dy, zd, isNaNd := corner(i+1, j+1, z)
 			if isNaNa || isNaNb || isNaNc || isNaNd {
 				continue
 			}
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' stroke='#FFFF00'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			z := int((za + zb + zc + zd) / 4 * zscale)
+			color := (z << 16) & (z - 255)
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' stroke='#%X'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color)
 		}
 	}
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int, f zf) (sx float64, sy float64, isNaN bool) {
+func corner(i, j int, f zf) (sx float64, sy float64, z float64, isNaN bool) {
 	// Find point (x,y) at corner of cell (i,j).
 	x, y := xy(i, j)
 	// Compute surface height z.
-	z := f(x, y)
+	z = f(x, y)
 
 	if math.IsNaN(z) {
 		isNaN = true
@@ -73,7 +75,7 @@ func corner(i, j int, f zf) (sx float64, sy float64, isNaN bool) {
 	sx = width/2 + (x-y)*cos30*xyscale
 	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
 
-	return sx, sy, isNaN
+	return sx, sy, z, isNaN
 }
 
 func xy(i, j int) (x, y float64) {
