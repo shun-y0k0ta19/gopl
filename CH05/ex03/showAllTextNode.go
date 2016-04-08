0e0d3adf0a22673f614c38f25da0df2c470abe3f
y0k0ta19 <motoyk@gmail.com>
2016-04-08 13:16:45 +0900

    remove comments

--- CH05/ex03/showAllTextNode.go
+++ CH05/ex03/showAllTextNode.go
@@ -19,21 +19,12 @@ func main() {
 	recursiveList := recursiveVisit(nil, doc)
 
 	for _, s := range recursiveList {
-		//s = ""
 		fmt.Println(s)
-		//fmt.Print(s)
 	}
 }
 
 func recursiveVisit(links []string, n *html.Node) []string {
 	if n != nil {
-		/*
-			fmt.Printf("n.Type : %d, TextNode :%d\n", n.Type, html.TextNode)
-			fmt.Printf("n.Namespace : %s\n", n.Namespace)
-			fmt.Printf("n.DataAtom : %x\n", n.DataAtom)
-			fmt.Printf("n.Attribute: %v\n", n.Attr)
-			fmt.Printf("n.Data: %v\n", n.Data)
-		*/
 		if n.Type == html.TextNode {
 			if n.Parent.Data != "script" && n.Parent.Data != "style" {
 				links = append(links, n.Data)
