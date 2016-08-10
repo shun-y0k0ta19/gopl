f78b18e13a99c0959afadfd695359f870658ca94
y0k0ta19 <motoyk@gmail.com>
2016-08-10 16:59:00 +0900

    not done

---
+++ CH10/ex01/myjpeg.go
@@ -0,0 +1,60 @@
+// Copyright © 2016 "Shun Yokota" All rights reserved
+
+// The jpeg command reads a PNG image from the standard input
+// and writes it as a JPEG image to the standard output.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png" // register PNG decoder
+	"io"
+	"os"
+)
+
+func main() {
+	var supportFlag bool
+	var formatFlag string
+	flag.BoolVar(&supportFlag, "s", false, "List support format.")
+	flag.StringVar(&formatFlag, "f", "jpeg", "Specify format to convert.")
+	flag.Parse()
+	if supportFlag {
+		fmt.Println("jpeg")
+		fmt.Println("png")
+		fmt.Println("gif")
+	}
+	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func toJPEG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+}
+
+func toPNG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	return png.Encode(out, img)
+}
+
+func toGIF(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	return gif.Encode(out, img, &gif.Options{NumColors: 255, Quantizer: nil, Drawer: nil})
+}
