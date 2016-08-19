2b9f4689236e92eeccc36c78548aa08cb81ac924
y0k0ta19 <motoyk@gmail.com>
2016-08-19 19:22:07 +0900

    add CH11/ex02

--- CH11/ex02/intset_test.go
+++ CH11/ex02/intset_test.go
@@ -1,12 +1,90 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Copyright © 2016 "Shun Yokota" All rights reserved
 
 package intset
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
+
+const NUM = 200
+
+func TestAdd(t *testing.T) {
+	expected := "{1 9 144}"
+	var x IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	x.Add(144)
+	if expected != x.String() { // "{1 9 144}"
+		t.Errorf("expected: {1 9 144}, Actual: %s\n", x.String())
+	}
+	y := make(MapIntSet)
+	y.Add(1)
+	y.Add(144)
+	y.Add(9)
+	y.Add(144)
+	if expected != y.String() { // "{1 9 144}"
+		t.Errorf("expected: {1 9 144}, Actual: %s\n", x.String())
+	}
+
+}
+
+func TestHas(t *testing.T) {
+	var x IntSet
+	y := make(MapIntSet)
+	for i := 0; i < NUM; i++ {
+		x.Add(i)
+		y.Add(i)
+	}
+	for i := 0; i < 2*NUM; i++ {
+		xok := x.Has(i)
+		yok := y.Has(i)
+		if i < NUM {
+			if !xok {
+				t.Errorf("IntSet does not have %d.\n", i)
+			}
+			if !yok {
+				t.Errorf("MapIntSet does not have %d.\n", i)
+			}
+		} else {
+			if xok {
+				t.Errorf("IntSet has %d.\n", i)
+			}
+			if yok {
+				t.Errorf("MapIntSet has %d.\n", i)
+			}
+
+		}
+	}
+}
+
+func TestUnionWith(t *testing.T) {
+	expected := map[int]bool{
+		1:   true,
+		144: true,
+		9:   true,
+		42:  true,
+	}
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+
+	y.Add(9)
+	y.Add(42)
+
+	x.UnionWith(&y)
+	for i := 0; i < NUM; i++ {
+		_, ok := expected[i]
+		if ok != x.Has(i) {
+			t.Errorf("IntSet can not calculate union with %d.\n", i)
+		}
+	}
+}
 
 func Example_one() {
-	//!+main
+
 	var x, y IntSet
 	x.Add(1)
 	x.Add(144)
@@ -30,21 +108,27 @@ func Example_one() {
 	// true false
 }
 
-func Example_two() {
-	var x IntSet
+func TestUnionWithForMap(t *testing.T) {
+	expected := map[int]bool{
+		1:   true,
+		144: true,
+		9:   true,
+		42:  true,
+	}
+	x := make(MapIntSet)
+	y := make(MapIntSet)
 	x.Add(1)
 	x.Add(144)
 	x.Add(9)
-	x.Add(42)
 
-	//!+note
-	fmt.Println(&x)         // "{1 9 42 144}"
-	fmt.Println(x.String()) // "{1 9 42 144}"
-	fmt.Println(x)          // "{[4398046511618 0 65536]}"
-	//!-note
+	y.Add(9)
+	y.Add(42)
 
-	// Output:
-	// {1 9 42 144}
-	// {1 9 42 144}
-	// {[4398046511618 0 65536]}
+	x.UnionWith(y)
+	for i := 0; i < NUM; i++ {
+		_, ok := expected[i]
+		if ok != x.Has(i) {
+			t.Errorf("IntSet can not calculate union with %d.\n", i)
+		}
+	}
 }
