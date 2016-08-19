e8098f28dccec5a80cb94bf9287f66e60665baef
y0k0ta19 <motoyk@gmail.com>
2016-08-20 00:47:41 +0900

    change copy right

--- CH11/ex04/word_test.go
+++ CH11/ex04/word_test.go
@@ -1,6 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
+// Copyright © 2016 "Shun Yokota" All rights reserved
 package word
 
 import (
@@ -143,50 +141,3 @@ func TestRandomNonPalindromes(t *testing.T) {
 		}
 	}
 }
-
-//!-random
-
-/*
-// Answer for Exercicse 11.1: Modify randomPalindrome to exercise
-// IsPalindrome's handling of punctuation and spaces.
-
-// WARNING: the conversion r -> upper -> lower doesn't preserve
-// the value of r in some cases, e.g., µ Μ, ſ S, ı I
-
-// randomPalindrome returns a palindrome whose length and contents
-// are derived from the pseudo-random number generator rng.
-func randomNoisyPalindrome(rng *rand.Rand) string {
-	n := rng.Intn(25) // random length up to 24
-	runes := make([]rune, n)
-	for i := 0; i < (n+1)/2; i++ {
-		r := rune(rng.Intn(0x200)) // random rune up to \u99
-		runes[i] = r
-		r1 := r
-		if unicode.IsLetter(r) && unicode.IsLower(r) {
-			r = unicode.ToUpper(r)
-			if unicode.ToLower(r) != r1 {
-				fmt.Printf("cap? %c %c\n", r1, r)
-			}
-		}
-		runes[n-1-i] = r
-	}
-	return "?" + string(runes) + "!"
-}
-
-func TestRandomNoisyPalindromes(t *testing.T) {
-	// Initialize a pseudo-random number generator.
-	seed := time.Now().UTC().UnixNano()
-	t.Logf("Random seed: %d", seed)
-	rng := rand.New(rand.NewSource(seed))
-
-	n := 0
-	for i := 0; i < 1000; i++ {
-		p := randomNoisyPalindrome(rng)
-		if !IsPalindrome(p) {
-			t.Errorf("IsNoisyPalindrome(%q) = false", p)
-			n++
-		}
-	}
-	fmt.Fprintf(os.Stderr, "fail = %d\n", n)
-}
-*/
