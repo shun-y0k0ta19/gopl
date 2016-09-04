3bbd3532e140d94affc7ed198894167906cb8076
y0k0ta19 <motoyk@gmail.com>
2016-09-05 08:02:00 +0900

    impl float chan bool func

---
+++ CH12/ex03/sexpr_test.go
@@ -0,0 +1,80 @@
+// Copyright © 2016 "Shun Yokota" All rights reserved
+
+package sexpr
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	/*
+		type Movie struct {
+			Title, Subtitle string
+			Year            int
+			Actor           map[string]string
+			Oscars          []string
+			Sequel          *string
+		}
+		strangelove := Movie{
+			Title:    "Dr. Strangelove",
+			Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+			Year:     1964,
+			Actor: map[string]string{
+				"Dr. Strangelove":            "Peter Sellers",
+				"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+				"Pres. Merkin Muffley":       "Peter Sellers",
+				"Gen. Buck Turgidson":        "George C. Scott",
+				"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+				`Maj. T.J. "King" Kong`:      "Slim Pickens",
+			},
+			Oscars: []string{
+				"Best Actor (Nomin.)",
+				"Best Adapted Screenplay (Nomin.)",
+				"Best Director (Nomin.)",
+				"Best Picture (Nomin.)",
+			},
+		}
+	*/
+	// Encode it
+	//data, err := Marshal(strangelove)
+	data, err := Marshal(0.0123)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+	fmt.Printf("Marshal() = %s\n", data)
+
+	data, err = Marshal(5 + 3.3i)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+	fmt.Printf("Marshal() = %s\n", data)
+
+	data, err = Marshal(make(chan<- string))
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+	fmt.Printf("Marshal() = %s\n", data)
+
+	f := encode
+	data, err = Marshal(f)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+	fmt.Printf("Marshal() = %s\n", data)
+
+	w := bufio.NewWriter(os.Stdin)
+	data, err = Marshal(w)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+	fmt.Printf("Marshal() = %s\n", data)
+
+}
