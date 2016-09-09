35fe1e4b436ddbe6c9de536541eae62958506d4d
y0k0ta19 <motoyk@gmail.com>
2016-09-09 15:37:10 +0900

    check cyclic

--- CH13/cycle/cycle.go
+++ CH13/cycle/cycle.go
@@ -5,20 +5,28 @@ package main
 import "fmt"
 
 func main() {
-	type CycleSlice []CycleSlice
-	var cycleSlice CycleSlice
-	cycleSlice = append(cycleSlice, cycleSlice)
-	cycleSlice = make(CycleSlice, 1)
-	cycleSlice[0] = cycleSlice
-	for i := range cycleSlice {
-		fmt.Println(cycleSlice[i])
-	}
-	fmt.Printf("cycleSice: %v\n", cycleSlice)
+	type S []S
+	s := make(S, 1, 10)
+	s = append(s, s)
+	fmt.Println(len(s))
+	fmt.Printf("%p %p %p\n", s, s[0], s[0][0])
+	fmt.Println(s)
+	/*
+		type CycleSlice []CycleSlice
+		var cycleSlice CycleSlice
+		cycleSlice = append(cycleSlice, cycleSlice)
+		cycleSlice = make(CycleSlice, 1)
+		cycleSlice[0] = cycleSlice
+		for i := range cycleSlice {
+			fmt.Println(cycleSlice[i])
+		}
+		fmt.Printf("cycleSice: %v\n", cycleSlice)
 
-	type cycle struct {
-		value int
-		tail  *cycle
-	}
-	var c cycle
-	c = cycle{42, &c}
+		type cycle struct {
+			value int
+			tail  *cycle
+		}
+		var c cycle
+		c = cycle{42, &c}
+	*/
 }
